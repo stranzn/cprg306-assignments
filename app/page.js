997ad6bd8cb2b03a9d3368982763963606ddd9da
stranzu <nikstranz01@gmail.com>
2024-11-04 13:26:38 -0700

    Completed week 8 assignment

--- app/page.js
+++ app/page.js
@@ -14,6 +14,7 @@ export default function Home() {
         <li><Link href ="./week-5/" className={linkStyle}>Week 5 Assignment</Link></li>
         <li><Link href ="./week-6/" className={linkStyle}>Week 6 Assignment</Link></li>
         <li><Link href ="./week-7/" className={linkStyle}>Week 7 Assignment</Link></li>
+        <li><Link href ="./week-8/" className={linkStyle}>Week 8 Assignment</Link></li>
       </ul>
     </main>
   );
---
+++ app/week-8/page.js
@@ -0,0 +1,47 @@
+"use client";
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import MealIdeas from "./meal-ideas";
+import itemsData from "./items.json";
+import React, { useState } from "react";
+
+export default function Page() {
+  const [items, setItems] = useState(itemsData);
+  const [selectedItemName, setSelectedItemName] = useState("");
+
+  const handleAddItem = (newItem) => {
+    setItems([...items, newItem]);
+  };
+
+  const handleItemSelect = (item) => {
+    const itemNameVal = item.name
+      .replace(
+        /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+        ""
+      )
+      .trim();
+    const itemArr = itemNameVal.split(",");
+    const nameArr = itemArr[0].split(" ");
+    setSelectedItemName(nameArr[0]);
+
+    console.log(nameArr[0]);
+  };
+
+  console.log("Selected ingredient:", selectedItemName);
+
+  return (
+    <main>
+      <title>Week 8 Assignment</title>
+      <h1 className="mb-3 ml-3 mt-3 text-center text-4xl font-bold font-sans">
+        Shopping List
+      </h1>
+      <div className="flex justify-center">
+        <div className="flex flex-col">
+          <NewItem onAddItem={handleAddItem} />
+          <ItemList items={items} onItemSelect={handleItemSelect} />
+        </div>
+        <MealIdeas ingredient={selectedItemName} />
+      </div>
+    </main>
+  );
+}
